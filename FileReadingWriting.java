0146a14d0bda854570e98fb66781cdd2a84360b0
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-09-17 20:28:25 +0600

    Add files via upload

--- FileReadingWriting.java
+++ FileReadingWriting.java
@@ -5,6 +5,29 @@ import java.io.BufferedReader;
 
 public class FileReadingWriting {
     public static void main(String[] args) {
-        
+        String name = "M. Oly Mahmud";
+        int roll = 210614;
+        double gpa = 3.33;
+
+        try {
+            FileWriter writer = new FileWriter("info.txt");
+            writer.write(name + " " + roll + " " + gpa);
+            writer.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            FileReader reader = new FileReader("info.txt");
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
