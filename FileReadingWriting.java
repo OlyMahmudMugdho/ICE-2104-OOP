05c6dd2af0739fe897e056d540ead0a92bb5bd94
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-09-17 20:19:12 +0600

    Add files via upload

---
+++ FileReadingWriting.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+
+public class FileReadingWriting {
+    public static void main(String[] args) {
+        
+    }
+}
